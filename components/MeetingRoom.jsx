dc11a331d7f55bf0ec754a3736cf352c5fb2a2de
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-05-03 16:31:30 +0530

    onclose removed

--- components/MeetingRoom.jsx
+++ components/MeetingRoom.jsx
@@ -62,10 +62,7 @@ const MeetingRoom = () => {
       </div>
 
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls
-          onLeave={() => router.push("/")}
-          onClose={() => router.push("/")}
-        />
+        <CallControls onLeave={() => router.push("/")} />
 
         <DropdownMenu>
           <div className="flex items-center">
